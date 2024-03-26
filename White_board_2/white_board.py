f3c25a322cbb9c66be6c82c3d754c18fa390b6fa
derodriguezz <derodriguezz@unal.edu.co>
2024-03-26 17:01:27 -0500

    Recuperación de cambios no guardados en última actualización

--- White_board_2/white_board.py
+++ White_board_2/white_board.py
@@ -34,11 +34,42 @@ color_box = PhotoImage(file="bar_icon.png")
 Label(root, image=color_box, bg="#f2f3f5").place(x=10, y=15)
 
 # eraser
-eraser = PhotoImage(file="eraser.png"
+eraser = PhotoImage(file="eraser_icon.png")
+Button(root, image = eraser, bg = "#f2f3f5").place(x=30,y=450)
 
+###color
+colors = Canvas(root, bg = "#fff", width = 37, height = 300, bd=0)
+colors.place(x=30, y=60)
 
+def display_pallete():
+    id= colors.create_rectangle((10,10,30,30,), fill= "black")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('black'))
 
+    id= colors.create_rectangle((10,40,30,60,), fill= "white")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('white'))
 
+    id= colors.create_rectangle((10,70,30,90,), fill= "gray")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('gray'))
+
+    id= colors.create_rectangle((10,100,30,120,), fill= "brown")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('brown'))
+    
+    id= colors.create_rectangle((10,130,30,150,), fill= "red")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('red'))
+
+    id= colors.create_rectangle((10,160,30,180,), fill= "yellow")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('yellow'))
+
+    id= colors.create_rectangle((10,190,30,210,), fill= "green")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('green'))
+
+    id= colors.create_rectangle((10,220,30,240,), fill= "blue")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('blue'))
+
+    id= colors.create_rectangle((10,250,30,270,), fill= "purple")
+    colors.tag_bind(id, '<Button-1>', lambda x: show_color('purple'))
+
+display_pallete()
 
 # Mainscreen
 canvas = Canvas(root, width=930, height=500, background="white", cursor="hand2")
