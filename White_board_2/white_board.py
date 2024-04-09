b50efa2b2490fa85716624b6fd37dfb5b6ea2796
derodriguezz <derodriguezz@unal.edu.co>
2024-04-09 16:43:22 -0500

    Se limpia el código de funciones no utilizadas y se reorganiza la estructura del script

--- White_board_2/white_board.py
+++ White_board_2/white_board.py
@@ -5,19 +5,7 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 
-def create_rounded_rectangle(canvas, x1, y1, x2, y2, radius=25, **kwargs):
-    """Crea un rectángulo con esquinas redondeadas en el canvas dado."""
-    # Dibuja las esquinas redondeadas
-    canvas.create_arc(x1, y1, x1 + radius * 2, y1 + radius * 2, start=90, extent=90, style=ARC, **kwargs)
-    canvas.create_arc(x2 - radius * 2, y1, x2, y1 + radius * 2, start=0, extent=90, style=ARC, **kwargs)
-    canvas.create_arc(x2 - radius * 2, y2 - radius * 2, x2, y2, start=270, extent=90, style=ARC, **kwargs)
-    canvas.create_arc(x1, y2 - radius * 2, x1 + radius * 2, y2, start=180, extent=90, style=ARC, **kwargs)
-    # Dibuja los lados rectos
-    
-    canvas.create_line(x1 + radius, y1, x2 - radius, y1, **kwargs)
-    canvas.create_line(x1 + radius, y2, x2 - radius, y2, **kwargs)
-    canvas.create_line(x1, y1 + radius, x1, y2 - radius, **kwargs)
-    canvas.create_line(x2, y1 + radius, x2, y2 - radius, **kwargs)
+
 
 root = tk.Tk()
 
@@ -73,6 +61,41 @@ def my_callback(event):
     f_img=tk.PhotoImage(file = filename)
     my_img = canvas.create_image(event.x, event.y, image = f_img)
 
+
+
+
+# Mainscreen
+canvas = Canvas(root, width=930, height=500, background="white", cursor="hand2")
+canvas.place(x=100, y=10)
+
+
+
+# Draw
+canvas.bind('<Button-1>', locate_xy)
+canvas.bind('<B1-Motion>', addline)
+
+
+
+###Slider###
+
+current_value = tk.DoubleVar()
+def get_current_value():
+    return '{:.2f}'.format(current_value.get())
+
+def slider_changed(event):
+    value_label.configure(text=get_current_value())
+    
+slider = ttk.Scale(root, from_=0, to = 100, orient = "horizontal", command=slider_changed, variable=current_value)
+slider.place(x=30, y=530)
+
+value_label=ttk.Label(root, text=get_current_value())
+value_label.place(x=27, y= 550)
+
+
+
+
+
+
 # Icon
 image_icon = PhotoImage(file="pen_icon1.png")
 root.iconphoto(False, image_icon)
@@ -123,31 +146,6 @@ def display_pallete():
     colors.tag_bind(id, '<Button-1>', lambda x: show_color('purple'))
 
 display_pallete()
-
-# Mainscreen
-canvas = Canvas(root, width=930, height=500, background="white", cursor="hand2")
-canvas.place(x=100, y=10)
-
-canvas.bind('<Button-1>', locate_xy)
-canvas.bind('<B1-Motion>', addline)
-
-
-###Slider###
-
-current_value = tk.DoubleVar()
-def get_current_value():
-    return '{:.2f}'.format(current_value.get())
-
-def slider_changed(event):
-    value_label.configure(text=get_current_value())
-    
-
-slider = ttk.Scale(root, from_=0, to = 100, orient = "horizontal", command=slider_changed, variable=current_value)
-slider.place(x=30, y=530)
-
-
-value_label=ttk.Label(root, text=get_current_value())
-value_label.place(x=27, y= 550)
 
 
 root.mainloop()
